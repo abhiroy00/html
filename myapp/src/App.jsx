88ea553f9b923c0f7c55afd8cbde2a98612de7bf
abhiroy00 <abhi7464099@gmail.com>
2025-05-31 18:06:30 +0530

    first commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BeautifulCard from './component/BeautifulCard'
+
+export default function App() {
+  return (
+    <div>
+      <BeautifulCard></BeautifulCard>
+      
+    </div>
+  )
+}
---
+++ myapp/src/component/BeautifulCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Card.css"; // We'll create this CSS file next
+
+const BeautifulCard = ({ title, description, imageUrl }) => {
+  return (
+    <div className="card">
+      <div className="card-image-container">
+        <img src={imageUrl} alt={title} className="card-image" />
+      </div>
+      <div className="card-content">
+        <h3 className="card-title">{title}</h3>
+        <p className="card-description">{description}</p>
+        <button className="card-button">Learn More</button>
+      </div>
+    </div>
+  );
+};
+
+export default BeautifulCard;
